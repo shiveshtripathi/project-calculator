30f90385a70ca14392c3c3adbb771c9910e6c44b
Shivesh Kumar Tripathi <20072713+shiveshtripathi@users.noreply.github.com>
2025-01-16 03:43:03 +0530

    Function to store the operand values

--- script.js
+++ script.js
@@ -2,22 +2,35 @@ const buttonData = document.querySelectorAll('#button')
 const displayArithmetic = document.querySelector('.display-box')
 let firstOperand = 0
 let secondOperand = 0
+const isNumeric = (string) => /^[+-]?\d+(\.\d+)?$/.test(string)
 
 buttonData.forEach((button) => {
   button.addEventListener('click', () => {
     const buttonValue = button.textContent
     console.log(`${buttonValue} Button was clicked`)
     if (buttonValue === 'AC') resetCalculator()
-    else displayScreen(buttonValue)
+    else if (isNumeric(buttonValue) || buttonValue === '.') {
+      firstOperand(buttonValue)
+      displayScreen(buttonValue)
+      console.log(firstOperand)
+    } else displayScreen(buttonValue)
   })
 })
 
 function resetCalculator() {
   displayArithmetic.textContent = ''
-  let firstOperand = 0
-  let secondOperand = 0
+  firstOperand = 0
+  secondOperand = 0
 }
 
 function displayScreen(buttonValue) {
   displayArithmetic.textContent += buttonValue
+}
+
+function storeFirstOperandValue(buttonValue) {
+  firstOperand += buttonValue
+}
+
+function storeSecondOperandValue(buttonValue) {
+  secondOperand += buttonValue
 }
